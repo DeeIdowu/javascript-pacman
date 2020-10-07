7de0d61e647f82e8a83023f112df01494b055db0
Dennis Idowu <dennis_idowu@live.com>
2020-10-07 13:03:55 -0400

    added functionals and imported from setup to index.js as well as pulled queries from index.html to index.js

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Working!');
+import { LEVEL, OBJECT_TYPE } from './setup';
+
+//DOM Elements:
+const gameGrid = document.querySelector('#game');
+const scoreTable = document.querySelector('#score');
+const startButton = document.querySelector('#start');
+
+//Game Constants:
+const POWER_PILL_TIME = 10000; //milliseconds
+const GLOBAL_SPEED = 80; //ms
+
+//Initial Setup:
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false; // when pacman eats powerpill
+let powerPillTimer = null;
+
+//functions required:
+//Gameover
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {}
